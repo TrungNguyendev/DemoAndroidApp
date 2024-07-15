e4817b58bee64e75bfbe611cf44a2b67955a6c39
TrungNguyendev <nguyentrungitdev@gmail.com>
2024-07-15 23:18:42 +0700

    add splash screens

--- app/src/main/java/com/example/demoandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/demoandroidapp/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.demoandroidapp.ui.theme.DemoAndroidAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Thread.sleep(3000)
+        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             DemoAndroidAppTheme {
