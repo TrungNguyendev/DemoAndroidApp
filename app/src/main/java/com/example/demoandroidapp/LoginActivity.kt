51ccb2de2d5a1c94bc42b936983f3c3cc57db413
TrungNguyendev <nguyentrungitdev@gmail.com>
2024-07-16 20:56:47 +0700

    add login activity

---
+++ app/src/main/java/com/example/demoandroidapp/LoginActivity.kt
@@ -0,0 +1,25 @@
+package com.example.demoandroidapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+
+        Thread.sleep(3000)
+        installSplashScreen()
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
--- app/src/main/java/com/example/demoandroidapp/MainActivity.kt
+++ app/src/main/java/com/example/demoandroidapp/MainActivity.kt
@@ -18,8 +18,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Thread.sleep(3000)
-        installSplashScreen()
         enableEdgeToEdge()
         setContent {
             DemoAndroidAppTheme {
